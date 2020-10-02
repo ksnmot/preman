ec3a6bd1fea09e1e240120591c24be76467c5b3d
POIMON <>
2020-10-02 21:46:12 +0900

    とりあえず作って、fireabse.jsとaddressesフォルダを追加。一応表示される。nuxtconfigにimport firebaseすると落ちる。fireabseは---saveでpackage.js似記載済み

---
+++ plugins/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAMVxQf18iXkdCvzI17iS7x4o63fFbGScI',
+  authDomain: 'preman0531.firebaseapp.com',
+  databaseURL: 'https://preman0531.firebaseio.com',
+  projectId: 'preman0531',
+  storageBucket: 'preman0531.appspot.com',
+  messagingSenderId: '178464898309',
+  appId: '1:178464898309:web:d45853343f09dfc7560b6c',
+  measurementId: 'G-36RTCG6989',
+}
+firebase.initializeApp(firebaseConfig)
