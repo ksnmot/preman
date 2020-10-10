b1fcdaa29a69d8475509f642d65c77acde350754
POIMON <>
2020-10-10 16:52:38 +0900

    マンガリストのUIを少し寄せた。

--- pages/mangas/index.vue
+++ pages/mangas/index.vue
@@ -5,7 +5,30 @@
         <h1>マンガ一覧</h1>
       </v-flex>
       <v-flex xs12 mt-1 justify-center>
-        <v-data-table :headers="headers" :items="mangas">
+        <v-card class="smallFont">
+          <v-list-item row="4">
+            <v-col class="px-2" cols="6">Title</v-col>
+            <v-col class="px-2" cols="2">Read</v-col>
+            <v-col class="px-2" cols="2">Latest</v-col>
+            <v-col class="px-2" cols="2">Unread</v-col>
+          </v-list-item>
+        </v-card>
+        <v-card v-for="manga in mangas" v-bind:key="manga.id">
+          <nuxt-link
+            :to="{
+              name: 'mangas-manga_id-edit',
+              params: { manga_id: manga.id },
+            }"
+          >
+            <v-list-item class="smallFont" row="4">
+              <v-col cols="6">{{ manga.title }}</v-col>
+              <v-col cols="2">{{ manga.read }}</v-col>
+              <v-col cols="2">{{ manga.latest }}</v-col>
+              <v-col cols="2">{{ manga.unread }}</v-col>
+            </v-list-item>
+          </nuxt-link>
+        </v-card>
+        <!-- <v-data-table :headers="headers" :items="mangas">
           <template v-slot:[`item.action`]="{ item }">
             <nuxt-link
               :to="{
@@ -17,7 +40,7 @@
             </nuxt-link>
             <v-icon @click="deleteConfirm(item.id)">mdi-delete-circle</v-icon>
           </template>
-        </v-data-table>
+        </v-data-table> -->
       </v-flex>
       <v-flex xs12 mt-5 justify-center>
         <router-link :to="{ name: 'mangas-create' }">
@@ -64,5 +87,8 @@ export default {
 <style scoped lang="scss">
 a {
   text-decoration: none;
+}
+.smallFont {
+  font-size: 11px;
 }
 </style>
