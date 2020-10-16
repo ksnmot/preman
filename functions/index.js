203c97b8b39f56ad11c0a30341b17df36b70d116
POIMON <>
2020-10-17 00:08:53 +0900

    no message

--- functions/index.js
+++ functions/index.js
@@ -7,4 +7,3 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info('Hello logs!', { structuredData: true })
   response.send('Hello from Firebase!')
 })
-i
